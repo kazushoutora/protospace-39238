8b279673081a8f9e2d4fe45d351923f695e3814d
kazushoutora <tomoyo.sato0830@gmail.com>
2023-05-15 15:41:24 +0900

    initial commit

---
+++ db/migrate/20230510110235_create_prototypes.rb
@@ -0,0 +1,13 @@
+class CreatePrototypes < ActiveRecord::Migration[6.0]
+  def change
+    create_table :prototypes do |t|
+      ## Database authenticatable
+      t.string     :title,      null: false
+      t.text       :catch_copy, null: false
+      t.text       :concept,    null: false
+      t.references :user,       null: false
+      
+      t.timestamps null: false
+    end
+  end
+end
